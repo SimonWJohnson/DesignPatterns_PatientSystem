1872518c6d69e1047355943b629f9dd0e61b24e4
SimonWJohnson <simonwjohnson@gmail.com>
2026-05-31 00:48:00 +0930

    Configured PatientManagementSystem to use CompositePatientDatabaseLoader, db output loaded first as per FR2

--- PatientSystem/PatientManagementSystem.cpp
+++ PatientSystem/PatientManagementSystem.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <map>
 #include <tuple>
+#include <memory>
 
 #include "Patient.h"
 #include "PatientDatabaseLoader.h"
@@ -12,20 +13,66 @@
 #include "HospitalAlertSystemFacade.h"
 // add the Adapter as per FR1
 #include "PatientFileLoaderAdapter.h"
+// add the Composite as per FR2
+#include "CompositePatientDatabaseLoader.h"
 
 using namespace std;
+
+// FR2 - Composite Pattern
+// Create the default Patient loader used by the system
+// The CompositePatientDatabaseLoader allows multiple loaders
+// to be treated as a single AbstractPatientDatabaseLoader
+
+// The database loader is created first followed by the file loader adapter
+// this ensures Patients are loaded in the order specified by the requirements
+
+// Use an anonymous namespace visible only to the PatientManagementSystem class
+namespace 
+{
+	std::unique_ptr<AbstractPatientDatabaseLoader> createDefaultPatientLoader()
+	{
+		// Create the Composite loader
+		std::unique_ptr<CompositePatientDatabaseLoader> loader =
+			std::make_unique<CompositePatientDatabaseLoader>();
+
+		// Add the database loader first
+		// Patients from the database be loaded before file Patients
+		loader->addLoader(std::make_unique<PatientDatabaseLoader>());
+
+		// Add the file loader second
+		// The Adapter created in FR1 aloows file loading to be
+		// treated like any other AbstractPatientDatabaseLoader
+		loader->addLoader(
+			std::make_unique<PatientFileLoaderAdapter>("patients.txt")
+		);
+
+		// Return the Composite as an AbstractPatientDatabaseLoader
+		// This allows PatientManagementSystem to remain unaware of the specific loader implementation being used
+		return loader;
+	}
+}
+
 
 
 PatientManagementSystem::PatientManagementSystem() :
 	//_patientDatabaseLoader(std::make_unique<PatientDatabaseLoader>()),
+	
 	// FR1 Adapter Pattern
 	// Use the PatientFileLoaderAdapter so Patient data is loaded from a text file through
 	// the AbstractPatientDatabaseLoader interface.
 	// Switching loaders requires changing only this line
-	_patientDatabaseLoader(std::make_unique<PatientFileLoaderAdapter>("patients.txt")),
+	//_patientDatabaseLoader(std::make_unique<PatientFileLoaderAdapter>("patients.txt")),
+
+	// FR2 Composite Pattern
+	// Use a CompositePatientDatabaseLoader so multiple loaders (database and file)
+	// can be treated as a single AbstractPatientDatabaseLoader
+	// Switching between database only, file only, database & file can be achieved by changing
+	// only the loader configuration
+	_patientDatabaseLoader(createDefaultPatientLoader()),
 	_hospitalAlertSystem(std::make_unique<HospitalAlertSystemFacade>()),
 	_gpNotificationSystem(std::make_unique<GPNotificationSystemFacade>())
 {
+	// Initialise all loaders contained within the Composite
 	_patientDatabaseLoader->initialiseConnection();
 }
 
