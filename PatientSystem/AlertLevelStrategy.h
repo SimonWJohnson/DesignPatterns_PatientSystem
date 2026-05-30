4b5f7bf7e2ef47583c84d82f01e78833e99b5844
SimonWJohnson <simonwjohnson@gmail.com>
2026-05-31 02:02:06 +0930

    created concrete AlertLevel classes for Cordyceps Brain Infection, Kepral's Syndrome, Andromeda Strain

--- PatientSystem/AlertLevelStrategy.h
+++ PatientSystem/AlertLevelStrategy.h
@@ -1,3 +1,22 @@
 #pragma once
 
 #include "PatientAlertLevels.h"
+
+// Forward declare classes
+class Patient;
+class Vitals;
+
+// FR3 - Strategy Pattern
+// Defines the commone interface for all alert level algorithms
+// Each concrete strategy calculates an Alertlevel differently
+
+class AlertLevelStrategy {
+public:
+	// pure virtual keeps AlertLevelStrategy as an interface
+	virtual ~AlertLevelStrategy() = default;
+
+	virtual AlertLevel calculateAlertLevel(
+		const Patient& patient,
+		const Vitals& vitals
+	) const = 0;
+};
