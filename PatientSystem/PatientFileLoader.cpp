6b8aa8c4956fd060be03acc430618d5c3b12d37a
SimonWJohnson <simonwjohnson@gmail.com>
2026-05-30 13:34:51 +0930

    Added error handling for file not reachable/readable

--- PatientSystem/PatientFileLoader.cpp
+++ PatientSystem/PatientFileLoader.cpp
@@ -17,17 +17,23 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file
 	vector<Patient*> patients{};
 
     std::ifstream inFile(file);
-    if (inFile.is_open()) {
-        // TODO: load your file here
-        // Add error handling for file not reachable/readable
 
-        // Split the name field - LastName, FirstName
-
-        // Convert birthday string into tm structure
-
-        // Historical vitals are loaded here
-        // Alert levels should not be calculated for historical data***
+    // Add error handling for file not reachable/readable
+    if (!inFile.is_open()) {
+        cout << "Could not open patient file: " << file << endl;
+        return patients;
     }
+    
+    // TODO: load your file here
+    string line;
+    
+
+    // Split the name field - LastName, FirstName
+
+    // Convert birthday string into tm structure
+
+    // Historical vitals are loaded here
+    // Alert levels should not be calculated for historical data***
 
     return patients;
 }
