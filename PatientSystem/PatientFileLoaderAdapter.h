4123390d68365cdd9065e3edf88e5826efc230bd
SimonWJohnson <simonwjohnson@gmail.com>
2026-05-30 21:57:32 +0930

    PatientFileLoaderAdapter.h first pass declaration complete

--- PatientSystem/PatientFileLoaderAdapter.h
+++ PatientSystem/PatientFileLoaderAdapter.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "AbstractPatientDatabaseLoader.h"
+#include "PatientFileLoader.h"
+
+#include <string>
+
+// FR1
+// Adapter Pattern
+// Allows PatientFileLoader to be used anywhere an 
+// AbstractPatientDatabaseLoader is expected
+class PatientFileLoaderAdapter : public AbstractPatientDatabaseLoader {
+public:
+	PatientFileLoaderAdapter(const std::string& fileName);
+
+	// PatientManagementSystem expects the following interface
+	void initialiseConnection() override;
+	void loadPatients(std::vector<Patient*>& patientsIn) override;
+	void closeConnection() override;
+
+private:
+	PatientFileLoader _fileLoader;
+	std::string _fileName;
+};
