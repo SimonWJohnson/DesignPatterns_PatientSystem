ebfafa6159fb0d58eb9222edcf98ae748e74191e
SimonWJohnson <simonwjohnson@gmail.com>
2026-05-31 03:04:33 +0930

    AndromedaStrain and KepralsSyndrome .h files implemented

--- PatientSystem/AndromedaStrainALertLevelStrategy.h
+++ PatientSystem/AndromedaStrainALertLevelStrategy.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "AlertLevelStrategy.h"
+
+// Strategy for Andromeda Strain
+// ALert level is based on blood pressure
+class AndromedaStrainAlertLevelStrategy : public AlertLevelStrategy {
+public:
+	AlertLevel calculateAlertLevel(
+		const Patient& patient,
+		const Vitals& vitals
+	) const override;
+};
--- PatientSystem/KepralsSyndromeAlertLevelStrategy.h
+++ PatientSystem/KepralsSyndromeAlertLevelStrategy.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "AlertLevelStrategy.h"
+
+// Strategy for Kepral's Syndrome
+// ALert level is based on patient age and heart rate
+class KepralsSyndromeAlertLevelStrategy : public AlertLevelStrategy {
+public:
+	AlertLevel calculateAlertLevel(
+		const Patient& patient,
+		const Vitals& vitals
+	) const override;
+};
