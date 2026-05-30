40a9cdd5557697e2ce1bc46b84d2d0999263e483
SimonWJohnson <simonwjohnson@gmail.com>
2026-05-31 02:43:40 +0930

    CordycepsBrainInfectionAlertLevelStategy.h file implemented

--- PatientSystem/CordycepsBrainInfectionAlertLevelStrategy.h
+++ PatientSystem/CordycepsBrainInfectionAlertLevelStrategy.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "AlertLevelStrategy.h"
+
+// Strategy for Cordyceps Brain Infection
+// Alert level is based on respiratory rate
+class CordycepsBrainInfectionAlertLevelStategy : public AlertLevelStrategy {
+public:
+	AlertLevel calculateAlertLevel(
+		const Patient& patient,
+		const Vitals& vitals
+	) const override;
+};
+
