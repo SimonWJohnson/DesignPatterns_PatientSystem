dd5858de5b8dcc8d261f5bcb69767c6beb6d0208
SimonWJohnson <simonwjohnson@gmail.com>
2026-05-31 13:48:25 +0930

    PatientAlertObserver interface implementation

--- PatientSystem/PatientAlertObserver.h
+++ PatientSystem/PatientAlertObserver.h
@@ -1,1 +1,18 @@
 #pragma once
+
+// forward declaration
+class Patient;
+
+// FR4 - Observer Pattern
+// Define a common interface for objects that need to be notified
+// when a Patient's alert level changes
+
+class PatientAlertObserver {
+public:
+	// Destructor
+	virtual ~PatientAlertObserver() = default;
+
+	// Send Alert / Notification
+	// Override in implementation
+	virtual void update(const Patient& patient) = 0;
+};
