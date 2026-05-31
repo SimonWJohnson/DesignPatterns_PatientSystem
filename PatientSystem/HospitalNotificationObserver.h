928f16db89ee5ac4a66bb2e163a4cd245cabed27
SimonWJohnson <simonwjohnson@gmail.com>
2026-05-31 14:30:39 +0930

    implemented declaration for HospitalNotificationObserver

--- PatientSystem/HospitalNotificationObserver.h
+++ PatientSystem/HospitalNotificationObserver.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "PatientAlertObserver.h"
+#include "HospitalAlertSystemFacade.h"
+
+// FR4 = Observer Pattern
+// Observer that notifies the hospital alert system when 
+// a Patient reaches a concerning alert level
+
+class HospitalNotificationObserver : public PatientAlertObserver {
+public:
+	void update(const Patient& patient) override;
+
+private:
+	HospitalAlertSystemFacade _hospitalAlertSystem;
+};
